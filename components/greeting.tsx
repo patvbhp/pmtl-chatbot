f8ac048ca4298a6b74f67fe869f81225665ef39d
patvbhp <patvbhp@gmail.com>
2025-07-16 07:41:21 +0700

    Update greeting.tsx

--- components/greeting.tsx
+++ components/greeting.tsx
@@ -13,7 +13,7 @@ export const Greeting = () => {
         transition={{ delay: 0.5 }}
         className="text-2xl font-semibold"
       >
-        Hello there!
+        Cháo các Sư Huynh
       </motion.div>
       <motion.div
         initial={{ opacity: 0, y: 10 }}
@@ -22,7 +22,7 @@ export const Greeting = () => {
         transition={{ delay: 0.6 }}
         className="text-2xl text-zinc-500"
       >
-        How can I help you today?
+        Đệ có thể giúp gì được ạ?
       </motion.div>
     </div>
   );
